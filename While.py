b30c1e6fe67491aca7c36d038b94dcc42363cdbc
Dangana <dangana_ruth@yahoo.com>
2021-02-07 20:50:25 +0100

    second commit

---
+++ While.py
@@ -0,0 +1,18 @@
+White = int(input("enter a value"))
+Blue = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+Black = 0
+Green = 5
+while Green < 10:
+    #print(White)
+    if White > int(Blue[Black]):
+        print(int(Blue[Black]))
+        Black += 1
+    else:
+        Green = 15
+        print("there is nothing to print")
+
+
+
+
+
+
